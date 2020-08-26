edf941f8a6f41b15ecad10a50992c0bd5214bdcf
Phil Tietjen <tietjen.philip@gmail.com>
2020-08-26 00:17:23 -0400

    feat(HomePage): update content

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -10,28 +10,9 @@ const HomePage = () => {
           <span className="text-pink-600">Redwood JS</span>
         </h2>
         <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
-          cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat
-          aliqua.
+          This is a simple blog built using Redwood JS and styled with
+          TailwindCSS. Both I think are super cool certified!
         </p>
-        <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-          <div className="rounded-md shadow">
-            <a
-              href="#"
-              className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-pink-600 hover:bg-pink-500 focus:outline-none focus:border-red-700 focus:shadow-outline-red transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
-            >
-              Get started
-            </a>
-          </div>
-          <div className="mt-3 sm:mt-0 sm:ml-3">
-            <a
-              href="#"
-              className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-pink-700 bg-pink-100 hover:text-pink-600 hover:bg-pink-50 focus:outline-none focus:shadow-outline-red focus:border-red-300 transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
-            >
-              Live demo
-            </a>
-          </div>
-        </div>
       </div>
       <section className="mt-10">
         <BlogPostsCell />
